43dc27bee65d71844ba6c142878d2f9ff865bec0
rigolinux <rigoberto7567@gmail.com>
2024-02-15 14:41:03 -0600

    setup portfolio

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import CardHeader from './components/CardHeader.vue';
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper ">
+      Hola  a todos
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <div class="md:fixed top-40 left-5 overflow-auto flex items-center justify-center">
+    <CardHeader />
+  </div>
+  <main class="container p-15 md:columns-2">
+    <div class="md:min-h-100">
+
+    </div>
+    <section class="ml-auto " >
+      <router-view />
+    </section>
+  </main>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const array = ref([1, 2, 3, 4, 5])
+</script>
+
+<template>
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <h2 class="sr-only">Toda inas lista </h2>
+
+      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <a v-for="a in array" :key="a" class="group">
+          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+          
+            <img src="@/assets/logo.svg"  class="h-full w-full object-cover object-center group-hover:opacity-75" />
+          </div>
+          <h3 class="mt-4 text-sm text-gray-700">Toda inas lista </h3>
+          <p class="mt-1 text-lg font-medium text-gray-900">Datos</p>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>

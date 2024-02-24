a16e0227aad7b1743f5bea25fbb9fde1308bb070
rigolinux <rigoberto7567@gmail.com>
2024-02-23 21:11:59 -0600

    setup config Dialog Alert

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {  RouterView } from 'vue-router'
 import CardHeader from './components/CardHeader.vue';
 import SideBar from './components/SideBar.vue';
+import ConfigDialog from './components/ConfigDialog.vue';
 import { useI18n } from 'vue-i18n';
-import {useSideBarStore} from '@/stores/sideBar';
+import { useSideBarStore } from '@/stores/sideBar';
+import { useConfigStore } from '@/stores/config';
 
 const sidebar = useSideBarStore();
+const config = useConfigStore();
+
+config.getAllOptions();
 
 const { t, locale } = useI18n();
 locale.value = 'es';
@@ -22,6 +27,9 @@ locale.value = 'es';
         <div @click.prevent="sidebar.toggle" class="btn bg-teal-400">
           Abrir
         </div>
+        <div @click.prevent="config.handleConfigDialog" class="btn bg-red-700">
+          Dialogo 
+        </div>
       </nav>
     </div>
   </header>
@@ -35,9 +43,21 @@ locale.value = 'es';
     </section>
     <SideBar  />
   </main>
-  
+ 
+  <div 
+  class="fixed top-0 left-0 w-full h-full flex items-center justify-center transition-opacity duration-500 ease-in-out"
+  @click.self.prevent="config.handleConfigDialog"
+  v-show="config.configDialog"
+  style="background-color: rgba(255, 255, 255, 0.75)"
+  >
+    <ConfigDialog />
+  </div>
+
 </template>
 
 <style scoped>
+/* Puedes agregar esto en tu archivo CSS global o en una etiqueta <style> de componente */
+
+
 
 </style>

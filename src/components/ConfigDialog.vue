6259849a03417c0ad19c520da2799e61e0cba5c9
rigolinux <rigoberto7567@gmail.com>
2024-02-26 09:23:00 -0600

    setup configuration dialog for lenguajes and colors

--- src/components/ConfigDialog.vue
+++ src/components/ConfigDialog.vue
@@ -1,8 +1,40 @@
 <script setup lang="ts">
 import { XMarkIcon} from "@heroicons/vue/24/outline";
 import { useConfigStore } from '@/stores/config';
+import { ref } from 'vue';
+import { useI18n } from "vue-i18n";
 
 const config = useConfigStore();
+
+const { t, locale } = useI18n();
+
+const Lenguajes = ref([
+    { id: 1, name: 'Spanish', value: 'es' },
+    { id: 2, name: 'English', value: 'en' }, 
+]);
+
+const Colors = ref([
+    { id: 1, name: 'CusGreen', value: '#28e98c' , selected: true},
+    { id: 2, name: 'CusYellow', value: '#e4af12', selected: false}, 
+    { id: 3, name: 'CusOrange', value: '#fe6f1d', selected: false}, 
+    { id: 4, name: 'Cuslight_blue', value: '#14c5fd', selected: false},      
+    { id: 5, name: 'CusPurple', value: '#a14cfb', selected: false},
+    { id: 6, name: 'CusRed', value: '#f31313' , selected: false},
+    { id: 7, name: 'CusPink', value: '#ff99cc', selected: false},
+    { id: 8, name: 'CusGrey', value: '#c0c0c0', selected: false}, 
+    {id: 9, name: 'CusBlue', value: '#1338f3', selected: false},
+ 
+]);
+
+const setupSelectedColor = (id: number) => {
+    Colors.value.forEach((color) => {
+        if (color.id === id) {
+            color.selected = true;
+        } else {
+            color.selected = false;
+        }
+    });
+};
 
 </script>
 
@@ -11,18 +43,36 @@ const config = useConfigStore();
         <div v-show="config.configDialog" class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div class="flex flex-col items-end mr-2">
             <button class="btn" @click.prevent="config.handleConfigDialog" >
-                <XMarkIcon class="w-6 h-6 text-black" />
+                <XMarkIcon class="w-6 h-6 text-white" />
             </button>
             </div>
-            <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            </a>
+            
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ t('Config_Page') }}</h5>
+
+            <div>
+                <label for="lenguaje" class="block text-sm font-medium  text-gray-700 dark:text-gray-400">Lenguaje</label>
+                <select v-model="locale" id="lenguaje" name="lenguaje" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 text-white bg-black focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md">
+                    <option v-for="lenguaje in Lenguajes" :key="lenguaje.id" :value="lenguaje.value">{{ lenguaje.name }}</option>
+                </select>
+            </div>
+
+            <div class="mt-4">
+                <label for="color" class="block text-sm font-medium  text-gray-700 dark:text-gray-400">Color</label>
+                <div class="flex justify-between items-center">
+                    <div v-for="color in Colors" @click="setupSelectedColor(color.id)" 
+                    :key="color.id" class="w-8 h-8 rounded-full cursor-pointer flex justify-center items-center" :style="{ backgroundColor: color.value, border: color.selected ? '2px solid #000' : 'none', }"
+                    >
+                    <div v-if="color.selected" class="  w-1 h-1  bg-black rounded-full">
+
+                    </div>
+                  </div>
+                </div>
+            </div>
+
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Read more
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
+                
             </a>
         </div>
     </transition>

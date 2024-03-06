a79ebe50004cd8a2db19b7e2d3fe74e7d9877976
rigolinux <rigoberto7567@gmail.com>
2024-03-06 12:35:28 -0600

    adding beatifuls menus

--- src/layout/MainView.vue
+++ src/layout/MainView.vue
@@ -4,7 +4,7 @@ import CardHeader from '@/components/CardHeader.vue';
 
 import { useI18n } from 'vue-i18n';
 import { useSideBarStore } from '@/stores/sideBar';
-import { SunIcon, MoonIcon, Cog8ToothIcon} from "@heroicons/vue/24/outline";
+import { SunIcon, MoonIcon, Cog8ToothIcon, Bars3Icon } from "@heroicons/vue/24/outline";
 import { useConfigStore } from '@/stores/config';
 
 
@@ -29,8 +29,19 @@ document.documentElement.style.setProperty('--text-color', config.textColor );
 
 <template>
     <div :style="config.Theme == 'Dark' ? 'background-color:black' : 'background-color:white'" >
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <div class="md:fixed top-40 left-5 overflow-auto flex items-center justify-center">
+        </div>
+        <CardHeader />
+        <main class="md:container md:p-15 ">
+        
+        <section class="md:w-1/2 md:ml-auto  sm:w-full">
+            <router-view />
+        </section>
+    
+        </main>
+        <header>
+     
       <div class="wrapper ">
         {{ t('welcome') }}
         <div class="fixed top-10  right-5
@@ -41,27 +52,19 @@ document.documentElement.style.setProperty('--text-color', config.textColor );
         <SunIcon v-if="config.Theme == 'Dark'"  @click.prevent="config.setTheme('Light');"  class="w-8 h-8 text-black hoverable"    />
         <MoonIcon v-if="config.Theme == 'Light'"  @click.prevent="config.setTheme('Dark'); "  class="w-8 h-8 text-white hoverable"     />
         </div>
-        <nav>
-          <div @click.prevent="sidebar.toggle" class="btn bg-teal-400">
-            Abrir
-          </div>
-          <div @click.prevent="config.handleConfigDialog" class="btn bg-red-700">
-            <Cog8ToothIcon class="w-8 h-8 bg" :style="'hover color:'+ config.textColor" />
-          </div>
-        </nav>
+        <div class="fixed top-10 left-5 flex 
+          md:left-15 lg:left-20">
+            <div @click.prevent="sidebar.toggle" class="dark:text-white w-10 h-10 text-center flex justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <Bars3Icon class="w-8 h-8 hoverable" />
+            </div>
+     
+            <div @click.prevent="config.handleConfigDialog" class=" dark:text-white ml-3 w-10 h-10 text-center flex justify-center items-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <Cog8ToothIcon class="w-8 h-8 hoverable" />
+            </div>
+        </div>
+      
       </div>
     </header>
-    <div class="md:fixed top-40 left-5 overflow-auto flex items-center justify-center">
-    </div>
-    <CardHeader />
-    <main class="md:container md:p-15 ">
-    
-      <section class="md:w-1/2 md:ml-auto  sm:w-full">
-        <router-view />
-      </section>
-   
-    </main>
-   
   </div>
 </template>
 

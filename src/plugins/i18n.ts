9a89f7d2001e75732b164e14b862fc99369a2e23
rigolinux <rigoberto7567@gmail.com>
2024-03-11 10:34:56 -0600

    add some ne translations

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -7,8 +7,9 @@ export const i18n = createI18n({
         en: {
             welcome: 'Welcome ',
             Config_Page: 'Config Page ',
-            description_1: 'this is my portfolio of projects',
-            description_2: 'My name is Bryan Portillo ',
+            description_1: 'Software Engineer',
+            description_2: 'my name is',
+            Rigo:'Rigoberto',
             Spanish: 'Spanish',
             English: 'English',
             Save: 'Save',
@@ -16,11 +17,15 @@ export const i18n = createI18n({
             Text_Color: 'Text Color',
             Navigation: 'Navigation',
             SoftEng: 'Software Engineer',
+            description_3: 'I design and code beautifully simple things and i love what i do. Just simple like that!',
+            years_experience: 'Years of experience',
+            completed_projects: 'Completed Projects',
         },
         es: {
             welcome: 'Bienvenido ',
-            description_1: 'este es mi portafolio de proyectos',
-            description_2: 'Mi nombre es Bryan Portillo ',
+            description_1: 'Software Engineer',
+            description_2: 'mi nombre is ',
+            Rigo:'Rigoberto',
             Config_Page: 'Configuracion de pagina ',
             Spanish: 'Español',
             English: 'Ingles',
@@ -29,6 +34,8 @@ export const i18n = createI18n({
             Text_Color: 'Color de Texto',
             Navigation: 'Navegacion',
             SoftEng: 'Ingeniero de Software',
+            description_3: 'Diseño y codifico cosas simples y me encanta cunado lo hago. ¡Asi de simple!',
+            years_experience: 'Años de experiencia',
         }
     }
 })

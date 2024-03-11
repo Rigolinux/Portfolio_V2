8a2ec7983f83c1cacc376c9e553bc34a511ed6c9
rigolinux <rigoberto7567@gmail.com>
2024-03-11 10:35:57 -0600

    add some transition for my project{

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { i18n } from './plugins/i18n'
+import  { MotionPlugin } from '@vueuse/motion'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(MotionPlugin)
 app.use(i18n)
 app.mount('#app')

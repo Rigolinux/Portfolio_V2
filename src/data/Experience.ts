c8f4b9fc200f4c35e8a11f03bcd344d246bedb62
rigolinux <rigoberto7567@gmail.com>
2024-03-13 09:37:11 -0600

    add example of expirience

---
+++ src/data/Experience.ts
@@ -0,0 +1,17 @@
+export const Experience = [
+    {
+        period: '2019 - Present',
+        header: 'Software Engineer',
+        description: 'Developed and maintained the company’s website',
+    },
+    {
+        period: '2017 - 2019',
+        header: 'Junior Software Engineer',
+        description: 'Developed and maintained the company’s website',
+    },
+    {
+        period: '2015 - 2017',
+        header: 'Intern',
+        description: 'Developed and maintained the company’s website',
+    }
+];

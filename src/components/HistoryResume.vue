e7e05e7b11ed5f5b7c93ed0cace36b36a3237c4b
rigolinux <rigoberto7567@gmail.com>
2024-03-13 09:37:37 -0600

    add dinamic component to work with this

---
+++ src/components/HistoryResume.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+
+
+interface Info {
+  period: string;
+  header: string;
+  description: string;
+}
+
+interface Props {
+    info: Info;
+}
+
+const props = defineProps<Props>()
+
+</script> 
+
+<template>
+
+<div class="parent" > 
+    <div class="flex" >
+        <div class="h-6 w-6 son-background rounded-full dark:bg-white bg-gray-700 opacity-15  "
+            
+        >
+        </div>
+        <div class="ml-3 son-color dark:text-white text-gray-700 "
+          
+        >
+            {{props.info.period}}
+        </div>
+    </div>
+    <div class="border-l-2 ml-3 border-gray-200 dark:border-gray-700  border-circle   dark:text-white ">
+        <div class="ml-8 ">
+            <br/>
+            <div class="text-xl">{{ props.info.header }} </div>
+            <div>
+                {{ props.info.description }}
+            </div>
+            <br/>
+        </div>
+    </div>
+</div>
+
+
+</template>
+
+
+<style scoped>
+
+
+</style>

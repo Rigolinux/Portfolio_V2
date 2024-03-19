4b7802657791eb3cb3557f2d8d7eaaba3e993d9a
rigolinux <rigoberto7567@gmail.com>
2024-03-19 14:24:38 -0600

    add new icons font

---
+++ src/components/IconsMdi.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon'
+
+
+const props = defineProps({
+    type: {
+        type: String,
+        required: false,
+        default: 'mdi',
+    },
+    path: {
+        type: String,
+        required: true,
+    },
+});
+console.log(props.type)
+</script>
+
+<template>
+     <svg-icon :size="34" :type="props.type" :path="props.path"></svg-icon>
+</template>

f23f179aa7e4a6fcd6aa9a2d496cac8f57c37b3b
rigolinux <rigoberto7567@gmail.com>
2024-03-22 15:09:03 -0600

    add new tecnologies

--- src/data/Tecnologies.ts
+++ src/data/Tecnologies.ts
@@ -1,6 +1,6 @@
 export const Tecnologies = [
     {
-        name: 'React',
+        name: 'Supabase',
         image: 'https://dhujijuorqbcfrqbdjqo.supabase.co/storage/v1/object/public/PortfolioBucket/technologies/supabase-TAiY.png'
     },
     {
@@ -18,5 +18,17 @@ export const Tecnologies = [
     {
         name : 'Amazon Web Services',
         image: 'https://dhujijuorqbcfrqbdjqo.supabase.co/storage/v1/object/public/PortfolioBucket/technologies/AWS-Emblem(1).png'
+    },
+    {
+        name: 'React/React Native',
+        image: 'https://dhujijuorqbcfrqbdjqo.supabase.co/storage/v1/object/public/PortfolioBucket/technologies/React-icon.svg.png'
+    },
+    {
+        name: 'Firebase',
+        image: 'https://dhujijuorqbcfrqbdjqo.supabase.co/storage/v1/object/public/PortfolioBucket/technologies/png-transparent-firebase-hd-logo.png?t=2024-03-22T21%3A06%3A19.999Z'
+    },
+    {
+        name: 'docker',
+        image: 'https://dhujijuorqbcfrqbdjqo.supabase.co/storage/v1/object/public/PortfolioBucket/technologies/png-transparent-docker-logo-kubernetes-microservices-cloud-computing-dockers-logo-text-logo-cloud-computing.png'
     }
 ];

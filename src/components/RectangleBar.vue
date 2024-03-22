9fe54cfcc1fe34e0306082d4a10a302ca4fb0872
rigolinux <rigoberto7567@gmail.com>
2024-03-22 11:37:30 -0600

    recreate a dinamic component

--- src/components/RectangleBar.vue
+++ src/components/RectangleBar.vue
@@ -1,29 +1,35 @@
 
 <script setup lang="ts"> 
 import IconsMdi from '@/components/IconsMdi.vue';    
-import { mdiLanguageTypescript } from '@mdi/js';
 import { ref,watch } from 'vue';
 import { useElementVisibility } from '@vueuse/core'
+import { useConfigStore } from '@/stores/config';
 
+interface Lenguajes {
+  name: string;
+  domain: number;
+  color: string;
+  icon: string;
+}
 
+const prop = defineProps<Lenguajes>();
 
-
-
+const config = useConfigStore();
 const dinamicWidth = ref(0);
-const Bar = ref(null);
-const element = useElementVisibility(Bar);
-
-watch(element, (value) => {
+const element = ref(null);
+const visibility = useElementVisibility(element);
+console.log(prop.color);
+watch(visibility, (value) => {
   if (value) {
     let i = 0;
     const interval = setInterval(() => {
-      if (i < 45) {
+      if (i < prop.domain) {
         dinamicWidth.value = i;
         i++;
       } else {
         clearInterval(interval);
       }
-    }, 1000 / 45);
+    }, 1000 / prop.domain);
   }
 });
 
@@ -36,15 +42,15 @@ watch(element, (value) => {
 </script>
 <template>
 
-<div v-motion-slide-visible-bottom ref="Bar">
-  <div class="flex justify-between mb-1" >
-    <span class="text-base font-medium text-blue-700 dark:text-white flex justify-between">
-       <IconsMdi :path="mdiLanguageTypescript"/>
-        <span class="ml-2 mt-2">TypeScript</span>
+<div v-motion-slide-visible-bottom ref="element">
+  <div class="flex justify-between mb-1 mt-3" >
+    <span class="text-base  font-medium  flex justify-between" :class="config.Theme == 'Dark' ? 'text-white' : prop.color">
+       <IconsMdi :path="prop.icon"/>
+        <span class="ml-2 mt-2 dark:text-white text-black">{{ prop.name }}</span>
       </span>
-    <span class="text-sm font-medium text-blue-700 dark:text-white">45%</span>
+    <span class="text-sm font-medium dark:text-white">{{prop.domain}}%</span>
   </div>
-  <div class="w-full bg-gray-200 rounded-full h-5 dark:bg-gray-700">
+  <div class="w-full  bg-gray-200 rounded-full h-5 dark:bg-gray-700">
     <div class="custom-bg  h-5 rounded-full" :style="`width: ${dinamicWidth}%`"></div>
   </div>
 </div>

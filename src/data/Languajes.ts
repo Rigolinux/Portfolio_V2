ae6172de634890684b176445fc5fda914ad1ee26
rigolinux <rigoberto7567@gmail.com>
2024-03-22 11:37:45 -0600

    list of my languajes

---
+++ src/data/Languajes.ts
@@ -0,0 +1,40 @@
+import { mdiLanguageJavascript, mdiLanguageCsharp,mdiLanguageTypescript, mdiLanguagePython, mdiLanguageRust , mdiLanguagePhp  } from '@mdi/js';
+export const Languajes = [
+    {
+        name: 'JavaScript',
+        icon: mdiLanguageJavascript,
+        color: 'ColJavaScript',
+        domain : 85
+    },
+    {
+        name: 'TypeScript',
+        icon:mdiLanguageTypescript ,
+        color: 'ColTypeScript',
+        domain : 75
+    },
+    {
+        name : 'Python',
+        icon : mdiLanguagePython,
+        color : 'ColPython',
+        domain : 60
+    },
+    {
+        name: 'C#',
+        icon: mdiLanguageCsharp,
+        color:  'ColCSharp',
+        domain : 50
+    },
+    {
+        name: 'PHP',
+        icon: mdiLanguagePhp,
+        color: 'ColPHP',
+        domain : 50
+    },
+    {
+        name: 'Rust',
+        icon: mdiLanguageRust,
+        color: 'ColRust',
+        domain : 40
+    },
+   
+]

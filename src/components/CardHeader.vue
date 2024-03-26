cdd7007bab29c6b22fa198d9e2ac462dab10aab6
rigolinux <rigoberto7567@gmail.com>
2024-03-26 12:52:34 -0600

    add new btn contac me

--- src/components/CardHeader.vue
+++ src/components/CardHeader.vue
@@ -21,13 +21,13 @@ const openInNewTab = (url: string) => {
         <img src="https://dhujijuorqbcfrqbdjqo.supabase.co/storage/v1/object/public/PortfolioBucket/portfolioImages/1685065428105.jpg?t=2024-03-07T16%3A53%3A46.713Z"
          alt="profile" class="w-3/4 h-full object-cover rounded-full" />
     </div>
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        Read more
-        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-        </svg>
-    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 mt-3">{{ t('About_Me') }}</p>
+    <div class="flex justify-center">
+        <a href="#" class=" w-3/4 flex justify-center items-center px-3 py-2 font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {{ t('Contact') }}
+        
+        </a>
+    </div>
     <div class="flex justify-evenly items-center mt-4 dark:text-white text-gray-700 ">
            
         <div class="w-12 rounded-full hoverable-borders h-12 flex justify-center items-center border   hoverable hover:cursor-pointer "

1b1c20be2c5e46ebbd80af0c977140ddb6d179a3
rigolinux <rigoberto7567@gmail.com>
2024-04-02 09:41:22 -0600

    add new menu only for grat screans

---
+++ src/data/MenuOptions.ts
@@ -0,0 +1,34 @@
+import {mdiAccount, mdiHome,mdiCodeBlockTags,mdiWalletTravel ,mdiEmailBox,mdiAnimationOutline    }from '@mdi/js';
+export const MenuOptions = [
+    {   
+        name: 'Resume',
+        Icon: mdiAccount,
+        link: 'experience'
+    },
+    {
+        name: 'Home',
+        Icon: mdiHome,
+        link: 'home'
+    },
+    {
+        name: 'history',
+        Icon: mdiWalletTravel,
+        link: 'history'
+    },
+    {
+        name: 'Services',
+        Icon: mdiAnimationOutline,
+        link: 'especialization'
+    },
+    {
+        name: 'Languajes',
+        Icon: mdiCodeBlockTags,
+        link: 'languajes'
+    },  
+    {
+        name: 'Contact',
+        Icon: mdiEmailBox,
+        link: 'contact'
+    }
+
+];
---
+++ src/views/MenuView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import IconsMdi from '@/components/IconsMdi.vue';    
+import { MenuOptions } from '@/data/MenuOptions';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+</script>
+
+
+<template>
+    <div class="border border-gray-500 p-4 rounded-full">
+        <div v-for="(item, index) in MenuOptions" :key="index"  class="my-5">
+            <a class="h-18 w-12 dark:text-white  text-gray-500 hoverable hover:cursor-pointer "  
+            :title="t(item.name)"
+            :href="'#'+item.link" >
+            <IconsMdi :path="item.Icon"  />
+            </a>
+    </div>
+    </div>
+</template>

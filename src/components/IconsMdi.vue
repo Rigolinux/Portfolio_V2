f3e6a0722bd334b485404153ea71dcf05d2e130b
rigolinux <rigoberto7567@gmail.com>
2024-04-03 09:35:31 -0600

    add new feature to icons componente

--- src/components/IconsMdi.vue
+++ src/components/IconsMdi.vue
@@ -17,12 +17,18 @@ const props = defineProps({
         required: false,
         default: null,
     },
+    center: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    
 });
 
 </script>
 
 <template>
-    <div class="flex flex-col">
+    <div class="flex flex-col" :class="center ? ' items-center justify-center': ''">
         <svg-icon :size="34" :type="props.type" :path="props.path"></svg-icon>
         <h2 v-if="props.title" class="text-center text-sm font-semibold text-gray-700 dark:text-gray-400">{{ props.title }}</h2>
     </div>
